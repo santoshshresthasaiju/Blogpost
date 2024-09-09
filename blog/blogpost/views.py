fc3306634dedb33881b71a34dba5c3b1e1df8389
Santosh Shrestha <santoshshrestha002@gmail.com>
2024-09-09 14:19:47 +0545

    updating the changes

--- blog/blogpost/views.py
+++ blog/blogpost/views.py
@@ -6,38 +6,37 @@ from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Q
 from django.contrib import messages
+from django.urls import reverse
 
 # views for home page
 @login_required
 def home(request):
     return render(request, 'blogpost/home.html')
 
-
+# views for post list
 @login_required
 def post_list(request):
     query = request.GET.get('q', '')
-    if request.user.is_authenticated:      
-        if query:
-            posts = Post.objects.filter(
-                Q(title__icontains=query) | Q(content__icontains=query), 
-                author=request.user
-            )
-        else:
-            posts = Post.objects.all()
-    else:
-        posts =Post.objects.none()
-    #using pagination
-    paginator = Paginator(posts, 2)
+    posts = Post.objects.filter(
+        Q(title__icontains=query) | Q(content__icontains=query), 
+        author=request.user
+    ) if query else Post.objects.select_related('author').prefetch_related('comments').all()
+
+    # Pagination with dynamic adjustment of posts per page
+    posts_per_page = request.GET.get('posts_per_page', 4)  # Default to 4
+    paginator = Paginator(posts, posts_per_page)
     page_number = request.GET.get('page')
+    
     try:
-        posts =paginator.page(page_number)
+        posts = paginator.page(page_number)
     except PageNotAnInteger:
-        posts =paginator.page(1)
+        posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-        
-    return render(request, 'blogpost/post_list.html', {'posts':posts, 'query':query})
 
+    return render(request, 'blogpost/post_list.html', {'posts': posts, 'query': query})
+
+# views for post creation
 @login_required
 def post_create(request):
     if request.method == 'POST':
@@ -50,15 +49,13 @@ def post_create(request):
             return redirect('blogpost:post_list')
     else:
         post_form = PostForm()
-    return render(request, 'blogpost/post_create.html', {'post_form':post_form})
 
+    return render(request, 'blogpost/post_create.html', {'post_form': post_form})
+
+# views for post update
 @login_required
 def post_update(request, post_id):
-    post = Post.objects.get(id=post_id) 
-    
-    if request.user != post.author:
-        return HttpResponseForbidden("You are not allowed to update this post.")
-    
+    post = get_object_or_404(Post, id=post_id)
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
@@ -67,13 +64,15 @@ def post_update(request, post_id):
             return redirect('blogpost:post_list')
     else:
         form = PostForm(instance=post)
-    return render(request, 'blogpost/post_update.html', {'form':form, 'post':post})
 
+    return render(request, 'blogpost/post_update.html', {'form': form, 'post': post})
+
+# views for post detail with comments
 @login_required
 def post_detail(request, post_id):
-    post =  Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, id=post_id)
     comments = post.comments.all()
-    
+
     if request.method == 'POST':
         comment_form = CommentForm(request.POST)
         if comment_form.is_valid():
@@ -84,31 +83,31 @@ def post_detail(request, post_id):
             return redirect('blogpost:post_detail', post_id=post.id)
     else:
         comment_form = CommentForm()
+
     return render(request, 'blogpost/post_detail.html', {
-        'post':post,
+        'post': post,
         'comments': comments,
         'comment_form': comment_form
-        })        
+    })
 
+# views for post deletion
 @login_required
 def post_delete(request, post_id):
-    post = Post.objects.get(id=post_id)
-    
-    if request.user != post.author:
-        return HttpResponseForbidden("You are not allowed to delete this post.")
-    
+    post = get_object_or_404(Post, id=post_id)
     post.delete()
     messages.success(request, 'Post deleted successfully!')
     return redirect('blogpost:post_list')
 
+# views for comment deletion
 @login_required
 def delete_comment(request, comment_id):
     comment = get_object_or_404(Comment, id=comment_id)
-    
-    if request.user != comment.author:
-        return HttpResponseForbidden("You are not allowed to delete this post.")
-    
-    if comment.author == request.user:
-        comment.delete()
-        messages.success(request, 'Comment deleted successfully!')
-        return redirect('blogpost:post_detail', post_id=comment.post.id)
+
+    # Allow either comment author or post author to delete the comment
+    if request.user != comment.author and request.user != comment.post.author:
+        messages.error(request, "You are not allowed to delete this comment.")
+        return redirect(reverse('blogpost:post_detail', args=[comment.post.id]))
+
+    comment.delete()
+    messages.success(request, 'Comment deleted successfully!')
+    return redirect(reverse('blogpost:post_detail', args=[comment.post.id]))
